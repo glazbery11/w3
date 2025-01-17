25407f52f2e329605d1a582e49a6e8a3f952db87
PP2 1 lab <glazbery11@gmail.com>
2025-01-17 20:16:31 +0500

    final week1

---
+++ w3school.py
@@ -0,0 +1,74 @@
+print("Hello, World!")
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+import sys
+
+print(sys.version) # version and this is comment
+
+
+'''
+big 
+comment
+
+
+
+gsfsg
+gsgsg
+'''
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+#good var names
+#Legal variable names:
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+x = "awesome"
+
+
+def myfunc():
+  print("Python is " + x)
+
+
+myfunc()
+
+x = 5
+print(type(x))
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+
+
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2")  # w will be 4.2
